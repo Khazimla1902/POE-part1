19f0fde482cd75780cd114ad4b9c6b7fb8f63913
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-09-11 13:11:16 +0000

    Conditions and requirements changes

--- src/main/java/com/mycompany/poepart1/PoePart1.java
+++ src/main/java/com/mycompany/poepart1/PoePart1.java
@@ -12,26 +12,32 @@ public class PoePart1 {
 
     public static void main(String[] args) {
         //Declarations
-        String Username, firstName, lastName;
+        String Username, firstName, lastName, Password;
         Scanner sc= new Scanner(System.in);
         //prompt the user
-        System.out.println("Please Enter Username: ");
+        System.out.print("Please Enter Username: ");
         Username=sc.next();
-        System.out.println("Please enter First Name: ");
+        System.out.print("Please enter First Name: ");
         firstName=sc.next();
-        System.out.println("Please enter Last Name: ");
+        System.out.print("Please enter Last Name: ");
         lastName=sc.next();
-       
-        
+       System.out.print("Please enter Password: ");
+        Password=sc.next();
         //Conditions and requirements
          
-        if(Username.contains("_")&& Username.length()>5){
+        if(Username.contains("_")&& Username.length()<5){
            System.out.println("Username Successfully caputured");
         }
         else{
             System.out.println("Username is not correctly formatted please ensure your username contains an underscore and is no more that 5 characters in length");
         }
         
+        if(Password.length() > 8 && Password.matches(".[A-Z].")){
+            System.out.println("Password successfully captured");
+        }
+        else{
+            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
+        }
         
         
     }
