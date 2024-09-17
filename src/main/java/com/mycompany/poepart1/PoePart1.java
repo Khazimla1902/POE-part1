c6565f50f18364cf1897e07c1d048b26f67dd841
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-09-17 17:27:03 +0000

    Calling external class

--- src/main/java/com/mycompany/poepart1/PoePart1.java
+++ src/main/java/com/mycompany/poepart1/PoePart1.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.poepart1;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 /**
  *
  * @author RC_Student_lab
@@ -11,50 +12,39 @@ import java.util.Scanner;
 public class PoePart1 {
 
     public static void main(String[] args) {
+                 login second = new login();
+
         //Declarations
         String Username, firstName, lastName, Password;
+        
+        
         Scanner sc= new Scanner(System.in);
         //prompt the user
+        do{
         System.out.print("Please Enter Username: ");
         Username=sc.next();
+        }while(second.Username(Username));
         System.out.print("Please enter First Name: ");
         firstName=sc.next();
+        
         System.out.print("Please enter Last Name: ");
         lastName=sc.next();
+        
         do{
        System.out.print("Please enter Password: ");
         Password=sc.next();
-        //Conditions and requirements
-        }while(!checkPassword(Password));  
         
-        if(Username.contains("_")&& Username.length()<5){
-           System.out.println("Username Successfully caputured");
-        }
-        else{
-            System.out.println("Username is not correctly formatted please ensure your username contains an underscore and is no more that 5 characters in length");
-        }
+        }while(!second.checkPassword(Password));
+        //calling my class
+         second.Username(Username);
+         second.checkPassword(Password);
+         
         
-        if(Password.length() > 8 && Password.matches(".[A-Z].")&& Password.matches(".[1-100].")&& Password.contains("~!@#$%^&*()_+?}{/")){
-            System.out.println("Password successfully captured");
-        }
-        else{
-            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
-        }
+        
+        
+        
         
         
     }
-    public static boolean checkPassword(String pass){
-        
-       boolean check=false; 
-       if(pass.length() > 8 && pass.matches(".[A-Z].")&& pass.matches(".[1-100].")&& pass.contains("~!@#$%^&*()_+?}{/")){
-            System.out.println("Password successfully captured");
-            check=true;
-        }
-        else{
-            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
-            check=false;
-        }
-        
-        return check;
-    }
+    
 }
