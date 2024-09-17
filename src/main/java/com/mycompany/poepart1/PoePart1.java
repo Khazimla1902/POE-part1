89ca31f15dcecfa2818e6168d01e6eea854a3b26
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-09-17 11:26:22 +0000

    do while loop

--- src/main/java/com/mycompany/poepart1/PoePart1.java
+++ src/main/java/com/mycompany/poepart1/PoePart1.java
@@ -21,10 +21,12 @@ public class PoePart1 {
         firstName=sc.next();
         System.out.print("Please enter Last Name: ");
         lastName=sc.next();
+        do{
        System.out.print("Please enter Password: ");
         Password=sc.next();
         //Conditions and requirements
-         
+        }while(!checkPassword(Password));  
+        
         if(Username.contains("_")&& Username.length()<5){
            System.out.println("Username Successfully caputured");
         }
@@ -32,7 +34,7 @@ public class PoePart1 {
             System.out.println("Username is not correctly formatted please ensure your username contains an underscore and is no more that 5 characters in length");
         }
         
-        if(Password.length() > 8 && Password.matches(".[A-Z].")){
+        if(Password.length() > 8 && Password.matches(".[A-Z].")&& Password.matches(".[1-100].")&& Password.contains("~!@#$%^&*()_+?}{/")){
             System.out.println("Password successfully captured");
         }
         else{
@@ -40,5 +42,19 @@ public class PoePart1 {
         }
         
         
+    }
+    public static boolean checkPassword(String pass){
+        
+       boolean check=false; 
+       if(pass.length() > 8 && pass.matches(".[A-Z].")&& pass.matches(".[1-100].")&& pass.contains("~!@#$%^&*()_+?}{/")){
+            System.out.println("Password successfully captured");
+            check=true;
+        }
+        else{
+            System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
+            check=false;
+        }
+        
+        return check;
     }
 }
