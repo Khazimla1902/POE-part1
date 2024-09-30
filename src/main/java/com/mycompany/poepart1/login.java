d46ce43e67ce5f8146f989e84b30ece8fa5ddbe1
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-09-30 13:37:14 +0000

    login system

--- src/main/java/com/mycompany/poepart1/login.java
+++ src/main/java/com/mycompany/poepart1/login.java
@@ -5,7 +5,7 @@ package com.mycompany.poepart1;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 import java.util.regex.Pattern;
-
+import java.util.Scanner;
 /**
  *
  * @author RC_Student_lab
@@ -21,18 +21,18 @@ public class login {
     
     
     public boolean checkPassword(String pass){
-        String regex="^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[@$!%#?&]).{8,}$";
+        String regex="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*#?&]).{8,}$";
        boolean check=false; 
        
        
        
        if(pass.matches(regex)){
-           System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
-           check=false;
+           System.out.println("Password succesfully captured");
+           
        }
         else{
-           System.out.println("Password successfully captured");
-           check=true;
+           System.out.println("Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a numberand a special character");
+           
            
        }
         
@@ -42,37 +42,41 @@ public class login {
     } 
     public boolean Username(String user){
         boolean check=false;
+   
+        
+       
+        
         if(user.contains("_")&& user.length()<=5){
-           System.out.println("Username Successfully caputured");
+          
+            System.out.println("Username Successfully caputured");
+           
         }
         else{
             System.out.println("Username is not correctly formatted please ensure your username contains an underscore and is no more that 5 characters in length");
+            
            
         }
         return check;
     }
     
-    public boolean loginUser(){
-        String Username="Asa_";
-        String Password="@Kozangawe1";
+    public boolean loginUser( String login, String loginPass, String loginUser){
+        
+        boolean check = false;
         String text1="Welcome ";
         String text=" it is great to see you again"; 
         String FirstName="Khazimla ";
         String lastName=" Dalasile";
-        boolean check=true;
-                
+
         
-        if(Username.equals("Asa_")&& Password.equals("@Kozangawe1")){                 
-       System.out.print(text1 + FirstName + lastName + text);
-        check=true;
-        }
-        else{
-            System.out.print("Username or password incorrect, please try again");
-        check=false;
-        }
-        return check;
-        
-        
+       if(login.equals(loginPass)&& login.equals(loginUser)) {
+           System.out.print(text1 + FirstName +lastName+ text);
+           
+       }
+       else{
+           System.out.print("Username or password incorrect, please try again");
+           
+       }
+      return check;
     }
     
 }
