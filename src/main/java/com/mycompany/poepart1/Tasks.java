b7be262f59fe37befa172f7af65088b370890671
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-10-20 10:22:42 +0000

    task description and task id

--- src/main/java/com/mycompany/poepart1/Tasks.java
+++ src/main/java/com/mycompany/poepart1/Tasks.java
@@ -17,4 +17,25 @@ class Tasks {
     private String taskID;
     private String taskStatus;
     
+    public Tasks(String taskName, int taskNumber, String taskDescription, String developerDetails, int taskDuration) {
+        this.taskName = taskName;
+        this.taskNumber = taskNumber;
+        this.taskDescription = taskDescription;
+        this.developerDetails = developerDetails;
+        this.taskDuration = taskDuration;
+        this.taskID = createTaskID();
+        this.taskStatus = "To Do";
+        
 }
+   //checking task description
+public boolean checkTaskDescription(){
+            return taskDescription.length() <= 50;
+   }
+// create and return the taskID
+    public String createTaskID() {
+        String taskNamePrefix = taskName.length() >= 2 ? taskName.substring(0, 2).toUpperCase() : taskName.toUpperCase();
+        String developerSuffix = developerDetails.length() >= 3 ? 
+            developerDetails.substring(developerDetails.length() - 3).toUpperCase() : 
+            developerDetails.toUpperCase();
+        return taskNamePrefix + ":" + taskNumber + ":" + developerSuffix;
+    }
