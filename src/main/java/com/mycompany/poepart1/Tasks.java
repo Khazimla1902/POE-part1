970c31ba0ce9bdf3a90cce2628d002d0c7f47e86
RC_Student_lab <RC_Student_lab@10.0.0.127>
2024-10-20 10:21:11 +0000

    task external file

---
+++ src/main/java/com/mycompany/poepart1/Tasks.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poepart1;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+class Tasks {
+     private String taskName;
+    private int taskNumber;
+    private String taskDescription;
+    private String developerDetails;
+    private int taskDuration;
+    private String taskID;
+    private String taskStatus;
+    
+}
